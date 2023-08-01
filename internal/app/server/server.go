27f67acc7071861d5d7ffde795560913fed9d8ac
t67y110v <likogawe@gmail.com>
2023-08-01 15:39:47 +0400

    fix: method type switch

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -45,7 +45,7 @@ func (s *server) configureRouter() {
 	// localhost:4000/user/register
 
 	s.router.Get("/swagger/*", swagger.HandlerDefault)
-	s.router.Get("/parse/:querry", s.handlers.Parse())
+	s.router.Post("/parse", s.handlers.Parse())
 	s.router.Post("/parse/all", s.handlers.ParserAll())
 	///////// USER GROUP ///////////////
 	////////////////////////////////////
